26897f17bef6f2cc1d831d286d68371f7b26d4c9
RobinFischer <robin.fischer@stud.h-da.de>
2018-03-06 11:13:47 +0100

    adapt button layout

--- src/pages/userpage/userpage.ts
+++ src/pages/userpage/userpage.ts
@@ -12,6 +12,14 @@ import { DeveloperPage } from "../developerpage/developerpage";
 @Component({
   selector: "user-page",
   templateUrl: "userpage.html"
+  // this tells the tabs component which Pages
+  // should be each tab's root Page
+  
+  //tab1Root = HomePage;
+  //tab2Root = Page2;
+  //tab3Root = Page3;
+
+
 })
 export class UserPage {
   // this is the important part for working with the ionic stack navigation
